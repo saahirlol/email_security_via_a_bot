bd5ed13bb42583d25203290ba3e2233bbfe95219
saahir <33964080+saahirlol@users.noreply.github.com>
2023-05-04 17:21:15 +0000

    mod setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 echo "Get a OpenAI key @ https://platform.openai.com/account/api-keys"
 echo "Enter your OpenAI key: "
 read openai_key
-
+pip3 install openai
 sed -i "7s/.*/openai.api_key = '$openai_key'/" main.py
